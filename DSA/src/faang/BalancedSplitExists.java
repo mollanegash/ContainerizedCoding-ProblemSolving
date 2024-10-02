b2abcbdee2ee73f1d6c0005c1d3b3f1f2428826a
mollanegash <mdraya3@gmail.com>
2024-10-01 20:09:09 -0500

    Create balancedSplitExists function to determine if an integer array can be split into two subsequences with equal sums and strict element ordering. Includes sorting and cumulative sum logic.

---
+++ DSA/src/faang/BalancedSplitExists.java
@@ -0,0 +1,41 @@
+package faang;
+
+import java.util.Arrays;
+
+public class BalancedSplitExists {
+	
+	public static void main(String[] args) {
+		System.out.println(balancedSplitExists(new int[] {1,5,7,1}));
+	
+		
+		
+	}
+	  static boolean balancedSplitExists(int[] arr) {
+		    // Write your code here
+		    
+		    //sum of A[]+B[]equal and A[index]<B[index]
+		    Arrays.sort(arr);
+		    int totalSum=0;
+		    for(int num:arr){
+		      totalSum+=num;
+		}
+		       // If the total sum is odd, we can't split it into two equal parts
+		        if (totalSum % 2 != 0) {
+		            return false;
+		        }
+		    int sumA=0;
+		    int length=arr.length;
+		    for(int i=0;i<length-1;i++){
+		      sumA+=arr[i];
+		      int sumB=totalSum-sumA;
+		      if(sumA==sumB && arr[i]<arr[i+1]){
+		        return true;
+		}
+		      
+		  
+		}
+		    return false;
+		    
+		  }
+
+}
