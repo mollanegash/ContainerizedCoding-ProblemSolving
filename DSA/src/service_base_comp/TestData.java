7721f665a88fe52f7cb47de46be5d9eb077deacc
mollanegash <mdraya3@gmail.com>
2024-10-11 13:32:23 -0500

    Print each employee using method reference

--- DSA/src/service_base_comp/TestData.java
+++ DSA/src/service_base_comp/TestData.java
@@ -7,33 +7,35 @@ import java.util.stream.Collectors;
 
 public class TestData {
 
-	    public static void main(String[] args) {
-	        // Creating Employee objects
-	        Employee e1 = new Employee(101, "CHETAN", 3000);
-	        Employee e2 = new Employee(102, "AMAR", 2000);
-	        Employee e3 = new Employee(103, "BHASKAR", 1500);
+	public static void main(String[] args) {
+		// Creating Employee objects
+		Employee e1 = new Employee(101, "CHETAN", 3000);
+		Employee e2 = new Employee(102, "AMAR", 2000);
+		Employee e3 = new Employee(103, "BHASKAR", 1500);
 
-	        // Creating a HashMap to store Employee details
-	        HashMap<Integer, Employee> employeeMap = new HashMap<>();
-	        employeeMap.put(e1.getId(), e1);
-	        employeeMap.put(e2.getId(), e2);
-	        employeeMap.put(e3.getId(), e3);
-	        
-	        employeeMap.values().stream()
-	        .sorted(Comparator.comparing(Employee::getName))  // Sort by name
-	        .collect(Collectors.toList())                     // Collect sorted results into a list
-	        .forEach(emp -> System.out.println(emp));          // Print each employee
+		// Creating a HashMap to store Employee details
+		HashMap<Integer, Employee> employeeMap = new HashMap<>();
+		employeeMap.put(e1.getId(), e1);
+		employeeMap.put(e2.getId(), e2);
+		employeeMap.put(e3.getId(), e3);
 
-	        // Get employee details in sorted order by employee name
-	        List<Employee> sortedEmployees = employeeMap.values().stream()
-	            .sorted(Comparator.comparing(Employee::getName))  // Sort by name
-	            .collect(Collectors.toList());  // Collect sorted results to a list
+		employeeMap.values().stream().sorted(Comparator.comparing(Employee::getName)) // Sort by name
+				.collect(Collectors.toList()) // Collect sorted results into a list
+				.forEach(emp -> System.out.println(emp)); // Print each employee
+		// or
+		employeeMap.values().stream().sorted(Comparator.comparing(Employee::getName)) // Sort by name
+				.forEach(System.out::println); // Print each employee using method reference
+//or
 
-	        // Print the sorted employee details
-	        for (Employee emp : sortedEmployees) {
-	            System.out.println(emp);
-	        }
-	    }
+		// Get employee details in sorted order by employee name
+		List<Employee> sortedEmployees = employeeMap.values().stream().sorted(Comparator.comparing(Employee::getName)) // Sort
+																														// by
+																														// name
+				.collect(Collectors.toList()); // Collect sorted results to a list
+
+		// Print the sorted employee details
+		for (Employee emp : sortedEmployees) {
+			System.out.println(emp);
+		}
 	}
-
-
+}
