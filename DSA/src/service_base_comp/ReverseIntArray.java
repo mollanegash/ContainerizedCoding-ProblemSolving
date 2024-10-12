268d65d7bf031fdf89c701f1c4f2c62cc1c0588c
mollanegash <mdraya3@gmail.com>
2024-10-12 16:17:58 -0500

    Implement array reversal functionality in ReverseIntArray class
    - Added reverseIntArray method to reverse an integer array in place.
    - Implemented a while loop to swap elements from both ends of the array.
    - Updated main method to test the reverseIntArray method with sample
    input.
    - Cleaned up unused imports and commented code for clarity.

---
+++ DSA/src/service_base_comp/ReverseIntArray.java
@@ -0,0 +1,37 @@
+package service_base_comp;
+
+import java.util.Arrays;
+
+
+public class ReverseIntArray {
+	public static void main(String[] args) {
+		int arrNums[]=reverseIntArray(new int[]{1,23,4,5,6,7,});
+		System.out.println(Arrays.toString(arrNums));
+		
+		
+	}
+	
+	static int[] reverseIntArray(int array[]) {
+		int left=0;
+		int right=array.length-1;
+		while(left<right) {
+			int temp=array[left];
+			array[left]=array[right];
+			array[right]=temp;
+			left++;
+			right--;
+			//return array;
+			
+		}
+	
+		
+		
+		
+		return array;
+				
+	}
+	//or
+
+	
+
+}
