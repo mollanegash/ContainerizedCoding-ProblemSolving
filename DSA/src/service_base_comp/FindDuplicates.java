988db36fa31dd2d5407a1a71ae35ad0fec86bfe5
mollanegash <mdraya3@gmail.com>
2024-10-16 17:44:39 -0500

    Implemented a method to find and print duplicate integers in an array.

---
+++ DSA/src/service_base_comp/FindDuplicates.java
@@ -0,0 +1,26 @@
+package service_base_comp;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindDuplicates {
+	public static void main(String[] args) {
+		findDuplicates(new int[] { 1, 2, 3, 1, 2, 4, 5, 4 });
+
+	}
+
+	static void findDuplicates(int nums[]) {
+		HashSet<Integer> hSet = new HashSet<>();
+
+		for (int num : nums) {
+			if (!hSet.add(num)) {
+				System.out.print(num + " ");
+
+			}
+
+		}
+
+	}
+
+}
